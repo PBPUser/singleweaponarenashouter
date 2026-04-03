348770c4f7c213bfc81d9658cbea1c83fcf42e24
iwuwka <iwuwka@korfex.com>
2026-04-03 21:23:35 +0700

    origin

--- scripts/main/Player.cs
+++ scripts/main/Player.cs
@@ -4,155 +4,233 @@ using System.Diagnostics;
 
 public partial class Player : Entity
 {
-    float SPEED = 5.0f;
-    float SPEED_RUNNING = 7.5f;
-    float SPEED_DELTA = 20f;
-    float JUMP_VELOCITY = 4.5f;
-    float GRAVITY = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
-    float pitch = 0f;
-    const float MAX_PITCH = 89f;
-    const float SHAKE_RANGE = .4f;
-    const float RUNNING_SHAKE = 1.4f;
-    float cameraShake = 0f;
-    bool isRunning = false;
-    float currentSpeed = 0f;
-    Vector3 directionInput = new Vector3();
-    float runStamina = 5f;
-    float staminaRegenerationDelay = 0f;
-    bool canRun = true;
-    Vector2 cameraParalax = new Vector2();
+	float SPEED = 5.0f;
+	float SPEED_CROUCHING = 2.5f;
+	float SPEED_RUNNING = 7.5f;
+	float SPEED_DELTA = 20f;
+	float JUMP_VELOCITY = 4.5f;
+	float GRAVITY = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+	float pitch = 0f;
+	const float MAX_PITCH = 89f;
+	const float SHAKE_RANGE = .4f;
+	const float RUNNING_SHAKE = 1.4f;
+	const float CROUCHING_SPEED = 4f;
+	const float CROUCHING_LRNGTH = 1f;
+	const float CROUCHING_STRENGTH = 4f;
 
-    float RunStamina
-    {
-        get => runStamina;
-        set
-        {
-            if (value < 0 && canRun)
-            {
-                runStamina = 0;
-                canRun = false;
-                staminaRegenerationDelay = STAMINA_REGENERATION_DELAY;
-            }
-            else if (value >= MAX_STAMINA && !canRun)
-            {
-                runStamina = MAX_STAMINA;
-                canRun = true;
-            }
-            else
-                runStamina = Math.Clamp(value, 0, MAX_STAMINA);
-        }
-    }
+	static Vector3
+		COLLISION_POSITION_DEFAULT = new Vector3(0, 1, 0),
+		COLLISION_TRANSFORM_DEFAULT = Vector3.One,
+		CAMERA_POSITION_DEFAULT = new Vector3(0, 1.5f, -.25f),
+		COLLISION_POSITION_CROUCH = new Vector3(0, .25f, 0),
+		COLLISION_TRANSFORM_CROUCH = new Vector3(1, .5f, 1),
+		CAMERA_POSITION_CROUCH = new Vector3(0, .5f, -.25f);
 
-    const float STAMINA_REGENERATION_SPEED = 1.2f;
-    const float STAMINA_REGENERATION_DELAY = 5f;
-    const float MAX_STAMINA = 5f;
+	float cameraShake = 0f;
+	bool isRunning = false;
+	float currentSpeed = 0f;
+	Vector3 directionInput = new Vector3();
+	float runStamina = 5f;
+	float staminaRegenerationDelay = 0f;
+	bool canRun = true;
+	Vector2 cameraParalax = new Vector2();
+	bool isSneaking = false;
+	float sneaking = 0f;
 
+	bool isCrouching = false;
+	float crouching = 0f;
+	Vector3 crouchingTo = new Vector3(0, 0, 0);
 
-    [Export]
-    PlayerHud hud;
+	bool IsCrouching
+	{
+		get => isCrouching;
+		set
+		{
+			if (value)
+				crouching = CROUCHING_LRNGTH;
+			isCrouching = value;
+		}
+	}
 
-    [Export]
-    Camera3D playerCamera;
+	bool IsSneaking
+	{
+		get => isSneaking;
+		set
+		{
+			isSneaking = value;
+			if (isRunning)
+				isRunning = false;
+		}
+	}
 
-    [Export]
-    PauseMenu pause;
+	float RunStamina
+	{
+		get => runStamina;
+		set
+		{
+			if (value < 0 && canRun)
+			{
+				runStamina = 0;
+				canRun = false;
+				staminaRegenerationDelay = STAMINA_REGENERATION_DELAY;
+			}
+			else if (value >= MAX_STAMINA && !canRun)
+			{
+				runStamina = MAX_STAMINA;
+				canRun = true;
+			}
+			else
+				runStamina = Math.Clamp(value, 0, MAX_STAMINA);
+		}
+	}
 
-    [Export]
-    PlayerDeathUI deathUI;
+	const float STAMINA_REGENERATION_SPEED = 1.2f;
+	const float STAMINA_REGENERATION_DELAY = 5f;
+	const float MAX_STAMINA = 5f;
 
-    [Export]
-    float mouseSensitivity = 0.3f;
-
-    [Export]
-    Node3D boxWeapon;
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        Health = 100;
-        Input.MouseMode = Input.MouseModeEnum.Captured;
-    }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        if (Input.IsActionJustPressed("pause") && !IsDied)
-            pause.Visible = !pause.Visible;
-        hud.HealthBar.Value = Health;
-        hud.StaminaBar.Value = RunStamina;
-        base._Process(delta);
-    }
-
-    public override void Died(bool value)
-    {
-        GetTree().Paused = value;
-        Input.MouseMode = value ? Input.MouseModeEnum.Visible : Input.MouseModeEnum.Captured;
-        deathUI.Visible = value;
-    }
-
-    public override void _Input(Godot.InputEvent ev)
-    {
-        if (ev is InputEventMouseMotion ievmm)
-        {
-            RotateY(Mathf.DegToRad(-ievmm.Relative.X * mouseSensitivity));
-            cameraParalax.X -= pitch;
-            pitch = Mathf.Clamp(pitch + -ievmm.Relative.Y * mouseSensitivity, -MAX_PITCH, MAX_PITCH);
-            playerCamera.RotationDegrees = new Vector3(pitch, 0, 0);
-            cameraParalax.X += pitch;
-        }
-    }
-
-    public override void _PhysicsProcess(double delta)
-    {
-        float deltaF = (float)delta;
-        float vX = Velocity.X;
-        float vY = Velocity.Y;
-        float vZ = Velocity.Z;
-        if (IsOnFloor())
-        {
-            if (Input.IsActionJustPressed("jump"))
-                vY = JUMP_VELOCITY;
-            else
-                vY = 0f;
-        }
-        else
-            vY += -GRAVITY * (float)delta;
-        if (Input.IsActionPressed("run") && canRun)
-            isRunning = true;
-        if (!canRun)
-            isRunning = false;
-        if (isRunning)
-            RunStamina -= deltaF;
-        else if (staminaRegenerationDelay > 0)
-            staminaRegenerationDelay -= deltaF;
-        else
-            RunStamina += deltaF * STAMINA_REGENERATION_SPEED;
-        var inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
-        var direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-        if (inputDir.X == 0 && inputDir.Y == 0)
-        {
-            currentSpeed = Mathf.MoveToward(currentSpeed, 0, SPEED_DELTA * (float)delta);
-            cameraShake = Mathf.MoveToward(cameraShake, 0, SPEED_DELTA * (float)delta);
-            isRunning = false;
-        }
-        else
-        {
-            currentSpeed = Mathf.MoveToward(currentSpeed, isRunning ? SPEED_RUNNING : SPEED, SPEED_DELTA * (float)delta);
-            cameraShake = Mathf.MoveToward(cameraShake, 1, SPEED_DELTA * (float)delta);
-            directionInput = direction;
-        }
-        cameraParalax.X = Mathf.MoveToward(cameraParalax.X, 0, 4f * deltaF);
-        cameraParalax.Y = Mathf.MoveToward(cameraParalax.Y, 0, 4f * deltaF);
-        vX = directionInput.X * currentSpeed;
-        vZ = directionInput.Z * currentSpeed;
-        var rotation = boxWeapon.Rotation;
-        rotation.X = MathF.Sin((float)Time.GetTicksMsec() * .01f) * 0.1f * cameraShake;
-        rotation.X -= cameraParalax.X;
-        rotation.X = Math.Clamp(rotation.X, -1f, 1f);
-        boxWeapon.Rotation = rotation;
-        Velocity = new Vector3(vX, vY, vZ);
-        MoveAndSlide();
+	float shakingTime = 0;
 
 
-    }
+	[Export]
+	PlayerHud hud;
+
+	[Export]
+	Camera3D playerCamera;
+
+	[Export]
+	PauseMenu pause;
+
+	[Export]
+	PlayerDeathUI deathUI;
+
+	[Export]
+	float mouseSensitivity = 0.3f;
+
+	[Export]
+	Node3D boxWeapon;
+
+	[Export]
+	CollisionShape3D collision;
+
+	[Export]
+	Node3D cameraShakeNode;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Health = 100;
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionJustPressed("pause") && !IsDied)
+			pause.Visible = !pause.Visible;
+		hud.HealthBar.Value = Health;
+		hud.StaminaBar.Value = RunStamina;
+		hud.Health.Text = "" + IsCrouching;
+		base._Process(delta);
+	}
+
+	public override void Died(bool value)
+	{
+		GetTree().Paused = value;
+		Input.MouseMode = value ? Input.MouseModeEnum.Visible : Input.MouseModeEnum.Captured;
+		deathUI.Visible = value;
+	}
+
+	public override void _Input(Godot.InputEvent ev)
+	{
+		if (ev is InputEventMouseMotion ievmm)
+		{
+			RotateY(Mathf.DegToRad(-ievmm.Relative.X * mouseSensitivity));
+			cameraParalax.X -= pitch;
+			pitch = Mathf.Clamp(pitch + -ievmm.Relative.Y * mouseSensitivity, -MAX_PITCH, MAX_PITCH);
+			playerCamera.RotationDegrees = new Vector3(pitch, 0, 0);
+			cameraParalax.X += pitch;
+		}
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		float deltaF = (float)delta;
+		float vX = Velocity.X;
+		float vY = Velocity.Y;
+		float vZ = Velocity.Z;
+		isSneaking = Input.IsActionPressed("shift");
+		if (Input.IsActionJustPressed("crouch") && IsOnFloor() && !isCrouching && canRun)
+		{
+			IsCrouching = true;
+			crouchingTo = (Transform.Basis * new Vector3(0, 0, -1)).Normalized();
+		}
+		else if (IsCrouching)
+		{
+			RunStamina -= deltaF * 4;
+			crouching = Mathf.MoveToward(crouching, 0, deltaF);
+			if (crouching <= 0)
+				IsCrouching = false;
+		}
+		if (IsOnFloor() && !isCrouching)
+		{
+			if (Input.IsActionJustPressed("jump") && !isSneaking)
+				vY = JUMP_VELOCITY;
+			else
+				vY = 0f;
+		}
+		else
+			vY += -GRAVITY * (float)delta;
+		sneaking = Mathf.MoveToward(sneaking, isSneaking ? 1 : 0, CROUCHING_SPEED * deltaF);
+		if (Input.IsActionPressed("run") && canRun)
+			isRunning = true;
+		if (!canRun)
+			isRunning = false;
+		if (isRunning)
+			RunStamina -= deltaF;
+		else if (staminaRegenerationDelay > 0)
+			staminaRegenerationDelay -= deltaF;
+		else
+			RunStamina += deltaF * STAMINA_REGENERATION_SPEED;
+		var inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
+		inputDir.Y *= 1 - (sneaking / 2);
+		var direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (inputDir.X == 0 && inputDir.Y == 0)
+		{
+			currentSpeed = Mathf.MoveToward(currentSpeed, 0, SPEED_DELTA * (float)delta);
+			cameraShake = Mathf.MoveToward(cameraShake, 0, SPEED_DELTA * (float)delta);
+			isRunning = false;
+		}
+		else
+		{
+			currentSpeed = Mathf.MoveToward(currentSpeed, isRunning ? SPEED_RUNNING : isSneaking ? SPEED_CROUCHING : SPEED, SPEED_DELTA * (float)delta);
+			cameraShake = Mathf.MoveToward(cameraShake, isRunning ? 2 : 1, SPEED_DELTA * (float)delta);
+			directionInput = direction;
+			shakingTime += deltaF * 0.007f * cameraShake;
+		}
+		var rotationCamera = playerCamera.RotationDegrees;
+		rotationCamera.Z = Mathf.Sin(shakingTime) * cameraShake;
+		playerCamera.RotationDegrees = rotationCamera;
+		var shakeNodePos = cameraShakeNode.Position;
+		shakeNodePos.Y = Mathf.Cos(shakingTime) * cameraShake * 0.1f;
+		cameraShakeNode.Position = shakeNodePos;
+		playerCamera.Position = CAMERA_POSITION_DEFAULT * (1f - sneaking) + (CAMERA_POSITION_CROUCH * sneaking);
+		collision.Position = COLLISION_POSITION_DEFAULT * (1f - sneaking) + (COLLISION_POSITION_CROUCH * sneaking);
+		collision.Scale = COLLISION_TRANSFORM_DEFAULT * (1f - sneaking) + (COLLISION_TRANSFORM_CROUCH * sneaking);
+		cameraParalax.X = Mathf.MoveToward(cameraParalax.X, 0, 4f * deltaF);
+		cameraParalax.Y = Mathf.MoveToward(cameraParalax.Y, 0, 4f * deltaF);
+		if (IsCrouching)
+		{
+			vX = crouchingTo.X * crouching * CROUCHING_STRENGTH;
+			vZ = crouchingTo.Z * crouching * CROUCHING_STRENGTH;
+		}
+		else
+		{
+			vX = directionInput.X * currentSpeed;
+			vZ = directionInput.Z * currentSpeed;
+		}
+		var rotation = boxWeapon.GlobalRotation;
+		boxWeapon.Rotation = rotation;
+		Velocity = new Vector3(vX, vY, vZ);
+		MoveAndSlide();
+	}
 }
